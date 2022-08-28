ed46284a7ed213d9b9b3ca64028ced154a7f7469
KauaOrtiz <kauaortizz@gmail.com>
2022-08-28 20:34:49 -0300

    simple tweet with a image

--- main.py
+++ main.py
@@ -6,14 +6,9 @@ consumer_secret = ""
 access_token = ""
 access_token_secret = ""
 
-client = tweepy.Client(
-    consumer_key=consumer_key, consumer_secret=consumer_secret,
-    access_token=access_token, access_token_secret=access_token_secret
-)
-vid = cv2.VideoCapture(0)
-
-try:
-    tweet = client.create_tweet(text="testando...")
-    print(tweet)
-except: 
-    print("deu pau")
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+imagePath = "cinzinha.jpg"
+status = "fala pessoal, criei esse bot aqui inspirado no @PepitoTheCat pra registrar o tanto de gato que sobe na minha cama durante o dia, ta em construção ainda e vai demorar p ficar bom porque tenho que arrumar mt coisa mas espero que fique decente\n\no github é: https://github.com/KauaOrtiz/Bot-Gatitos"
+api.update_status_with_media(status=status,filename=imagePath)
