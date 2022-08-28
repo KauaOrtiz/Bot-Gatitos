94a200d3b63f0ab3570ff470f148468490720649
KauaOrtiz <kauaortizz@gmail.com>
2022-08-28 18:51:28 -0300

    add files

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import tweepy
+import cv2
 
-
-consumer_key = "G6EBx3VrsRPZ9fdvPv4Tro0aA"
-consumer_secret = "n43aOnlhJqFFbLmNEcY1sGGsw36tar4kc84UwuHAd3QulEGzbW"
-access_token = "1563536795031089152-GyrgLfkOpCC8ZB536TIGf5BPnKXOBd"
-access_token_secret = "0OFuuYU0FtQbIjo3RucUfRDOkZMPX0nnLolXTT9dDshVW"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
 client = tweepy.Client(
     consumer_key=consumer_key, consumer_secret=consumer_secret,
     access_token=access_token, access_token_secret=access_token_secret
 )
+vid = cv2.VideoCapture(0)
 
 try:
     tweet = client.create_tweet(text="testando...")
     print(tweet)
-except:
+except: 
     print("deu pau")
